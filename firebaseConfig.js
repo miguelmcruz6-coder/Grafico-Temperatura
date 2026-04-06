82d65b2208eab1873372a2a02a3ece8f4c4361e5
Miguel Miyaki da Cruz - SENAI <miguel.m.cruz6@aluno.senai.br>
2026-04-06 11:25:50 -0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,24 +1,22 @@
-
-// Import 
+// Import do Firebase modular
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-analytics.js";
-  
-// Firebase configurationl
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-database.js";
+
+// Config do seu projeto
 const firebaseConfig = {
   apiKey: "AIzaSyCVkXIDp9yWHdDRC4PKe4D7Nnx5ljmm2vE",
   authDomain: "grafico-teste-c7260.firebaseapp.com",
+  databaseURL: "https://grafico-teste-c7260-default-rtdb.firebaseio.com",
   projectId: "grafico-teste-c7260",
   storageBucket: "grafico-teste-c7260.firebasestorage.app",
   messagingSenderId: "825620153324",
-  appId: "1:825620153324:web:332292488461fb5b3ff6c9",
-  measurementId: "G-7623E5S7FK"
+  appId: "1:825620153324:web:332292488461fb5b3ff6c9"
 };
 
-// Initialize Firebase
+// Inicializa Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
+// 🔥 Realtime Database
+const db = getDatabase(app);
 
-// Exporta apenas o banco de dados
-export { database };
-
+export { db };
